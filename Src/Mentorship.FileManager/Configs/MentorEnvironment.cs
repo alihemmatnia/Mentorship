243a3e45a643b76d3bca20ba05046247e8fcd6a8
alihemmatnia <ali.hemmatnia@gmail.com>
2024-11-22 21:16:32 +0330

    implment file upload and download on s3

---
+++ Src/Mentorship.FileManager/Configs/MentorEnvironment.cs
@@ -0,0 +1,100 @@
+﻿using System.Security;
+
+namespace Mentorship.FileManager.Configs
+{
+    public class MentorEnvironment
+    {
+        public const string S3_ENDPOINT = "S3_ENDPOINT";
+        public const string S3_ACCESSKEY = "S3_ACCESSKEY";
+        public const string S3_SECRETKEY = "S3_SECRETKEY";
+        public const string S3_FILEBUCKET = "S3_FILEBUCKET";
+
+        public static string ReadVariable(string name)
+        {
+            try
+            {
+                return Environment.GetEnvironmentVariable(name);
+            }
+            catch (ArgumentNullException)
+            {
+                return null;
+            }
+            catch (SecurityException)
+            {
+                return null;
+            }
+        }
+
+        public static T? ReadVariable<T>(string name)
+        {
+            var value = ReadVariable(name);
+            if (value == null) return default;
+
+            return ChangeType<T>(value);
+        }
+
+        public static T ChangeType<T>(object value)
+        {
+            var t = typeof(T);
+
+            if (t.IsGenericType && t.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+            {
+                if (value == null)
+                {
+                    return default;
+                }
+
+                t = Nullable.GetUnderlyingType(t);
+            }
+
+            return (T)Convert.ChangeType(value, t);
+        }
+
+        public static string ReadRequiredVariable(string name)
+        {
+            try
+            {
+                var value = Environment.GetEnvironmentVariable(name);
+                if (value == null) throw new ArgumentNullException();
+                return value;
+            }
+            catch (ArgumentNullException)
+            {
+                throw new ArgumentNullException($"Environment Variable {name} Not Provided");
+            }
+            catch (SecurityException)
+            {
+                throw new SecurityException($"SecurityException - Environment Variable {name} Not Found");
+            }
+        }
+
+        public static T ReadRequiredVariable<T>(string name)
+        {
+            return (T)Convert.ChangeType(ReadRequiredVariable(name), typeof(T));
+        }
+
+        public static bool IsFromSettingFile()
+        {
+            try
+            {
+                return Environment.GetEnvironmentVariable("FromFile") == "true";
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool IsTestEnvironment()
+        {
+            try
+            {
+                return Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Test";
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Src/Mentorship.FileManager/Utils/S3Client.cs
@@ -0,0 +1,76 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Mentorship.FileManager.Utils
+{
+    public class S3Client
+    {
+        private static IAmazonS3 _s3Client;
+
+        public S3Client()
+        {
+            _s3Client = new AmazonS3Client(
+               Configs.Configs.S3_ACCESSKEY,
+                Configs.Configs.S3_SECRETKEY,
+                new AmazonS3Config
+                {
+                    ServiceURL = Configs.Configs.S3_ENDPOINT, 
+                    ForcePathStyle = true, 
+                });
+        }
+
+        public async Task<(Stream, string)> DownloadFile(
+            string bucketName,
+             string fileName)
+        {
+            GetObjectRequest request = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = fileName,
+            };
+            using GetObjectResponse response = await _s3Client.GetObjectAsync(request);
+            if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
+                return (null, null);
+
+            string contentType = response.Headers["Content-Type"];
+            using Stream responseStream = response.ResponseStream;
+
+            MemoryStream memoryStream = new MemoryStream();
+            await response.ResponseStream.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            return (memoryStream, contentType);
+        }
+
+
+        public async Task Delete(string bucketName, string fileName)
+        {
+            DeleteObjectRequest request = new DeleteObjectRequest
+            {
+                BucketName = bucketName,
+                Key = fileName,
+            };
+
+            await _s3Client.DeleteObjectAsync(request);
+        }
+
+        public async Task<bool> UploadFile(
+            string bucketName,
+             Stream file,
+             string fileName, string contentType)
+        {
+
+            var putRequest = new PutObjectRequest
+            {
+                BucketName = bucketName,
+                InputStream = file,
+                Key = fileName,
+                ContentType = contentType
+            };
+
+            putRequest.Metadata.Add("x-amz-meta-title", fileName);
+
+            PutObjectResponse response = await _s3Client.PutObjectAsync(putRequest);
+            return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+    }
+}
